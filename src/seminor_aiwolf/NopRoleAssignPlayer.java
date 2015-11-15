dab9dd15618ad5434919cf938e7428cec64db9a6
Haruhisa Ishida <haruhisa.ishida@gmail.com>
2015-11-15 15:36:25 +0900

    NopSeerをnopRoleAssignPlayerのseerロジックとして追加

--- src/seminor_aiwolf/NopRoleAssignPlayer.java
+++ src/seminor_aiwolf/NopRoleAssignPlayer.java
@@ -4,6 +4,10 @@ import org.aiwolf.client.base.player.AbstractRoleAssignPlayer;
 
 public class NopRoleAssignPlayer extends AbstractRoleAssignPlayer {
 
+	public NopRoleAssignPlayer() {
+		setSeerPlayer(new NopSeer());
+	}
+
 	@Override
 	public String getName() {
 		return "seminor_aiwolf";
