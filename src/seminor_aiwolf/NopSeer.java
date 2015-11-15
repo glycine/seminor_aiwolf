be2569a84ce8d77af0a85342d2e8c28ddb7a1d22
Haruhisa Ishida <haruhisa.ishida@gmail.com>
2015-11-15 15:50:05 +0900

    フォーマット修正

--- src/seminor_aiwolf/NopSeer.java
+++ src/seminor_aiwolf/NopSeer.java
@@ -14,11 +14,12 @@ import org.aiwolf.common.data.Talk;
 import org.aiwolf.common.net.GameInfo;
 
 public class NopSeer extends AbstractSeer {
-
-	boolean isComingOut = false;	//カミングアウトをしたかどうか
+	static final int CO_DAY = 3; // 最低でもCOする日
+	boolean isComingOut = false; // カミングアウトをしたかどうか
+	boolean todayCOTimingJudged = false; // 今日その日にCOするかを判定したかどうか
 	int readTalkNum = 0;
 
-	//偽占いCOしているプレイヤーのリスト
+	// 偽占いCOしているプレイヤーのリスト
 	List<Agent> fakeSeerCOAgent = new ArrayList<Agent>();
 
 	@Override
@@ -33,30 +34,31 @@ public class NopSeer extends AbstractSeer {
 
 	}
 
-	public void update(GameInfo gameInfo){
+	public void update(GameInfo gameInfo) {
 		super.update(gameInfo);
-		//今日のログを取得
+		// 今日のログを取得
 		List<Talk> talkList = gameInfo.getTalkList();
 
-		for(int i = readTalkNum; i < talkList.size(); i++){
+		for (int i = readTalkNum; i < talkList.size(); i++) {
 			Talk talk = talkList.get(i);
-			//発話をパース
+			// 発話をパース
 			Utterance utterance = new Utterance(talk.getContent());
 
-			//発話のトピックごとに処理
-			switch(utterance.getTopic()){
+			// 発話のトピックごとに処理
+			switch (utterance.getTopic()) {
 			case COMINGOUT:
-				//カミングアウトの発話の処理
-				//自分以外で占いCOしているプレイヤーの場合
-				if(utterance.getRole() == Role.SEER && !talk.getAgent().equals(getMe())){
+				// カミングアウトの発話の処理
+				// 自分以外で占いCOしているプレイヤーの場合
+				if (utterance.getRole() == Role.SEER
+						&& !talk.getAgent().equals(getMe())) {
 					fakeSeerCOAgent.add(utterance.getTarget());
 				}
 				break;
 			case DIVINED:
-				//占い結果の発話の処理
+				// 占い結果の発話の処理
 				break;
 			default:
-					break;
+				break;
 			}
 			readTalkNum++;
 		}
@@ -64,20 +66,22 @@ public class NopSeer extends AbstractSeer {
 
 	@Override
 	public String talk() {
-		if(isComingOut){
-			//占い結果報告
+		GameInfo gameInfo = this.getLatestDayGameInfo();
+		if (isComingOut) {
 			isComingOut = true;
-		}else{
-			for(Judge judge: getMyJudgeList()){
-				if(judge.getResult() == Species.WEREWOLF){
-					String comingoutTalk = TemplateTalkFactory.comingout(getMe(), getMyRole());
+		} else {
+			for (Judge judge : getMyJudgeList()) {
+				if (judge.getResult() == Species.WEREWOLF) {
+					String comingoutTalk = TemplateTalkFactory.comingout(
+							getMe(), getMyRole());
 					return comingoutTalk;
-				}else if(false){
-
-					String comingoutTalk = TemplateTalkFactory.comingout(getMe(), getMyRole());
+				} else if (false) {
+					String comingoutTalk = TemplateTalkFactory.comingout(
+							getMe(), getMyRole());
 					return comingoutTalk;
-				}else if(getLatestDayGameInfo().getDay() >= 3){
-					String comingoutTalk = TemplateTalkFactory.comingout(getMe(), getMyRole());
+				} else if (getLatestDayGameInfo().getDay() >= 3) {
+					String comingoutTalk = TemplateTalkFactory.comingout(
+							getMe(), getMyRole());
 					return comingoutTalk;
 				}
 			}
